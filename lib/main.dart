f833c50a82d17d67c640e9ab300a4f02c2d74278
abgm1941 <72179285+abgm1941@users.noreply.github.com>
2020-10-02 14:04:29 +0700

    Update AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Stay At Home'),
+          title: Text('Please Stay at Home'),
           centerTitle: true,
           backgroundColor: Colors.teal[900],
         ),
