a560aeb9459e21b1ad2780e529f904506d1560bd
westbdev <72179463+westbdev@users.noreply.github.com>
2020-10-02 13:38:43 +0700

    add Text Style (Font Size) on AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Stay At Home'),
+          title: Text('Stay At Home', style: TextStyle(fontSize: 20),),
           centerTitle: true,
           backgroundColor: Colors.teal[900],
         ),
